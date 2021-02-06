6c840fd6c805ddcc87e2c21d731d452cbead84da
Afterburner53 <azizcanguv@gmail.com>
2021-02-06 17:50:05 +0300

    06.02.2021 8.gün Sonu

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    { 
+        IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public void Add(Brand brand)
+        {
+            _brandDal.Add(brand);
+        }
+
+        public void Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brandDal.GetAll();
+
+        }
+
+        public List<Brand> GetCarsByBrandId(int id)
+        {
+            return _brandDal.GetAll(p => p.Id == id);
+        }
+
+        public void Update(Brand brand)
+        {
+            _brandDal.Update(brand);
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (ReCapProjectDatabase context = new ReCapProjectDatabase())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (ReCapProjectDatabase context = new ReCapProjectDatabase())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (ReCapProjectDatabase context = new ReCapProjectDatabase())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public List<Car> Get(Expression<Func<Car, bool>> filter)
+        {
+            using (ReCapProjectDatabase context = new ReCapProjectDatabase())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (ReCapProjectDatabase context = new ReCapProjectDatabase())
+            {
+                var modifiedEntity = context.Entry(entity);
+                modifiedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
