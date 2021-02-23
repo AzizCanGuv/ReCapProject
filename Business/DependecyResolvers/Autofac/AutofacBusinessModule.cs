9ab5992f65e1c4da50f6b73bcff096fdf57519ba
Afterburner53 <azizcanguv@gmail.com>
2021-02-23 20:31:00 +0300

    AOP structure is added.

--- Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
@@ -34,6 +37,13 @@ namespace Business.DependecyResolvers.Autofac
             builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
 
 
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
 
 
 
